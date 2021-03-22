73a81aa043b8c7de2c31897b7ae34cdef6aae6f1
Joma Dm <Joma_Dm@outlook.com>
2021-03-21 21:37:55 -0600

    Se corrigieron errores en archivo de cifrador afin

--- aff_cipher.py
+++ aff_cipher.py
@@ -1,60 +1,49 @@
 from file_manager import *
 
-n = {'EN':['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+
+n = {'EN': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
      'ES': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n','ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-     
-}
- 
- 
-#print (message)
+            'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+     }
 
-def cipher (EN,key,message):
-    mult = key [0]
-    cor = key [1]
-    
+
+def encrypt(EN, key, message):
+    mult = key[0]
+    cor = key[1]
+
     encrypted = []
- 
-    
-    #Obtiene el indice del mensaje 
-    for i,e in enumerate(message):
-      
-        formula = (mult*(EN.index(e))+ cor)%len(EN)
-        encrypted.append(EN[formula]) # para agregar un elemento al final
+
+    # Obtiene el indice del mensaje
+    for e in message:
+
+        formula = (mult*(EN.index(e)) + cor) % len(EN)
+        encrypted.append(EN[formula])  # para agregar un elemento al final
         #print ('letra: ',e,EN.index(e))
-    #print(encrypted)
+    # print(encrypted)
     cipher_text = ''.join(encrypted)
-    
-    write_file(cipher_text, file_name= 'encrypted_aff.txt')
-    
-cipher (n['EN'],[7,17],message)
+
+    write_file(cipher_text, file_name='encrypted_aff.txt')
 
 
+def decrypt(EN, key_inv, ciphertext):
 
-
-
-
-
-def decrypt (EN,key_inv,ciphertext):
-    
-
-    mult_in = key_inv [0]
+    mult_in = key_inv[0]
     cor_in = key_inv[1]
-    
+
     desencrypted = []
-    
-    for i,e in enumerate(ciphertext):
-        
-        formula_inv = (mult_in*(EN.index(e)+cor_in))%len(EN)
+
+    for e in ciphertext:
+
+        formula_inv = (mult_in*(EN.index(e)+cor_in)) % len(EN)
         desencrypted.append(EN[formula_inv])
-    
+
     #print (''.join(desencrypted))
-    
-
-    
-    write_file(cipher_text, file_name= 'decrypted_aff.txt')    
-decrypt (n['EN'],[15,9],'olqrxbeml')
+    plain_text = ''.join(desencrypted)
+    write_file(plain_text, file_name='decrypted_aff.txt')
 
 
-
+if __name__ == '__main__':
+    message = 'holamundo'
+    encrypt(n['EN'], [7, 17], message)
+    decrypt(n['EN'], [15, 9], 'olqrxbeml')
